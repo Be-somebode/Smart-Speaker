f6a9ef44e366c31a6ba5dc29515cb1f2f5599176
Shengbo HUANG <49448495+Be-somebode@users.noreply.github.com>
2021-04-14 12:35:28 +0800

    upload Qt Music Player (to play mp3)

---
+++ src/QtMusicPlayer/ui_mainwindow.h
@@ -0,0 +1,121 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.11.3
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTableWidget>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QGridLayout *gridLayout;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *ButtonLastMusic;
+    QPushButton *ButtonPause;
+    QPushButton *ButtonNext;
+    QTableWidget *tableWidget;
+    QLabel *label;
+    QMenuBar *menuBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(415, 493);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        gridLayout = new QGridLayout(centralWidget);
+        gridLayout->setSpacing(6);
+        gridLayout->setContentsMargins(11, 11, 11, 11);
+        gridLayout->setObjectName(QStringLiteral("gridLayout"));
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        ButtonLastMusic = new QPushButton(centralWidget);
+        ButtonLastMusic->setObjectName(QStringLiteral("ButtonLastMusic"));
+
+        horizontalLayout->addWidget(ButtonLastMusic);
+
+        ButtonPause = new QPushButton(centralWidget);
+        ButtonPause->setObjectName(QStringLiteral("ButtonPause"));
+
+        horizontalLayout->addWidget(ButtonPause);
+
+        ButtonNext = new QPushButton(centralWidget);
+        ButtonNext->setObjectName(QStringLiteral("ButtonNext"));
+
+        horizontalLayout->addWidget(ButtonNext);
+
+
+        gridLayout->addLayout(horizontalLayout, 1, 0, 1, 1);
+
+        tableWidget = new QTableWidget(centralWidget);
+        if (tableWidget->columnCount() < 1)
+            tableWidget->setColumnCount(1);
+        QTableWidgetItem *__qtablewidgetitem = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(0, __qtablewidgetitem);
+        tableWidget->setObjectName(QStringLiteral("tableWidget"));
+        tableWidget->horizontalHeader()->setStretchLastSection(true);
+
+        gridLayout->addWidget(tableWidget, 0, 0, 1, 1);
+
+        label = new QLabel(centralWidget);
+        label->setObjectName(QStringLiteral("label"));
+
+        gridLayout->addWidget(label, 2, 0, 1, 1);
+
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 415, 23));
+        MainWindow->setMenuBar(menuBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        ButtonLastMusic->setText(QApplication::translate("MainWindow", "<-", nullptr));
+        ButtonPause->setText(QString());
+        ButtonNext->setText(QApplication::translate("MainWindow", "->", nullptr));
+        QTableWidgetItem *___qtablewidgetitem = tableWidget->horizontalHeaderItem(0);
+        ___qtablewidgetitem->setText(QApplication::translate("MainWindow", "\346\255\214\346\233\262", nullptr));
+        label->setText(QApplication::translate("MainWindow", "\346\255\214\346\233\262", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
